a563eda7bdd08260b0b5a87aadbd792b97156bc3
Manuel <manuelheller@outlook.com>
2020-08-07 21:52:17 +0200

    load_data

--- shopping.py
+++ shopping.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -59,7 +61,53 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    df = pd.read_csv(filename)
+    # Convert month data in shopping dataset to numeric data.
+    conditions = [
+        df['Month'] == 'Jan',
+        df['Month'] == 'Feb',
+        df['Month'] == 'Mar',
+        df['Month'] == 'Apr',
+        df['Month'] == 'May',
+        df['Month'] == 'June',
+        df['Month'] == 'Jul',
+        df['Month'] == 'Aug',
+        df['Month'] == 'Sep',
+        df['Month'] == 'Oct',
+        df['Month'] == 'Nov',
+        df['Month'] == 'Dec'
+    ]
+    outputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    df['Month'] = np.select(conditions, outputs)
+    # Convert weekend data in shopping dataset to numeric data.
+    conditions = [df['Weekend'] == True]
+    outputs = [1]
+    df['Weekend'] = np.select(conditions, outputs, 0)
+    # Convert wisitor type data in shopping dataset to numeric data.
+    conditions = [df['VisitorType'] == 'Returning_Visitor']
+    outputs = [1]
+    df['VisitorType'] = np.select(conditions, outputs, 0)
+    # Convert wevenue data in shopping dataset to numeric data.
+    conditions = [df['Revenue'] == True]
+    outputs = [1]
+    df['Revenue'] = np.select(conditions, outputs, 0)
+
+    # Put information in tuple
+    evidence = []
+    label = []
+    for row in df.itertuples(): 
+        user_session = [
+            row.Administrative, row.Administrative_Duration, row.Informational,
+            row.Informational_Duration, row.ProductRelated, row.ProductRelated_Duration,
+            row.BounceRates, row.ExitRates, row.PageValues, row.SpecialDay, row.Month,
+            row.OperatingSystems, row.Browser, row.Region, row.TrafficType, row.VisitorType,
+            row.Weekend
+        ]
+        evidence.append(user_session)
+        label.append(row.Revenue)
+    shopping = (evidence, label)
+
+    return shopping
 
 
 def train_model(evidence, labels):
