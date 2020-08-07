7a03664f3b16f95accd61c5b54aee3d6a6be3314
Manuel <manuelheller@outlook.com>
2020-08-07 15:48:11 +0200

    load data progress

--- test.py
+++ test.py
@@ -6,40 +6,40 @@ data_file = 'shopping.csv'
 
 # Additional functions
 
-def numeric_conversion(shopping):
+def numeric_conversion(df):
     """
     Function that converts all non numeric data in shopping
     dataset to numeric data.
     """
     # Month data
     conditions = [
-    shopping['Month'] == 'Jan',
-    shopping['Month'] == 'Feb',
-    shopping['Month'] == 'Mar',
-    shopping['Month'] == 'Apr',
-    shopping['Month'] == 'May',
-    shopping['Month'] == 'June',
-    shopping['Month'] == 'Jul',
-    shopping['Month'] == 'Aug',
-    shopping['Month'] == 'Sep',
-    shopping['Month'] == 'Oct',
-    shopping['Month'] == 'Nov',
-    shopping['Month'] == 'Dec'
+    df['Month'] == 'Jan',
+    df['Month'] == 'Feb',
+    df['Month'] == 'Mar',
+    df['Month'] == 'Apr',
+    df['Month'] == 'May',
+    df['Month'] == 'June',
+    df['Month'] == 'Jul',
+    df['Month'] == 'Aug',
+    df['Month'] == 'Sep',
+    df['Month'] == 'Oct',
+    df['Month'] == 'Nov',
+    df['Month'] == 'Dec'
     ]
     outputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    shopping['Month'] = np.select(conditions, outputs)
+    df['Month'] = np.select(conditions, outputs)
     # Weekend
-    conditions = [shopping['Weekend'] == True]
+    conditions = [df['Weekend'] == True]
     outputs = [1]
-    shopping['Weekend'] = np.select(conditions, outputs, 0)
+    df['Weekend'] = np.select(conditions, outputs, 0)
     # Visitor type
-    conditions = [shopping['VisitorType'] == 'Returning_Visitor']
+    conditions = [df['VisitorType'] == 'Returning_Visitor']
     outputs = [1]
-    shopping['VisitorType'] = np.select(conditions, outputs, 0)
+    df['VisitorType'] = np.select(conditions, outputs, 0)
     # Revenue
-    conditions = [shopping['Revenue'] == True]
+    conditions = [df['Revenue'] == True]
     outputs = [1]
-    shopping['Revenue'] = np.select(conditions, outputs, 0)
+    df['Revenue'] = np.select(conditions, outputs, 0)
 
 
 
@@ -47,8 +47,20 @@ shopping_df = pd.read_csv(data_file)
 
 numeric_conversion(shopping_df)
 
-print(shopping_df.head())
+#print(shopping_df.head())
 
 evidence = []
 label = []
 shopping = (evidence, label)
+
+
+print(len(shopping_df['Administrative'].tolist()))
+
+tracker = 5
+for row in shopping_df:
+    #print(row)
+    evidence.append(row[0:17])
+    label.append(row[-1])
+    tracker += -1
+    if tracker < 0:
+            break
